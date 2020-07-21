51a3225ccad9a9a85b7870052f604fc1ca1382e4
Joshua Peterson <peterson_joshua@berkeley.edu>
2020-07-21 16:24:44 +0000

    array exercises complete

---
+++ arr_ex8.js
@@ -0,0 +1,15 @@
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+console.log(oddLengths(arr)); // => [1, 5, 3]
+
+
+function oddLengths(arr) {
+  return arr.reduce((acc, elem) => {
+    if (elem.length % 2 != 0) {
+      acc.push(elem.length);
+      return acc
+    } 
+    else {
+      return acc
+    } 
+  } , []);
+}
